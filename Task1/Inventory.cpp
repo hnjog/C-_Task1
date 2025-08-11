fb01a525358644e40a5fbe559d17c877d8698b09
Hyun <hnjogo@gmail.com>
2025-08-11 12:01:18 +0900

    Inventory class 제작 및 enum.h 생성

---
+++ Task1/Inventory.cpp
@@ -0,0 +1,112 @@
+﻿#include"Inventory.h"
+#include"Character.h"
+#include"Util.h"
+
+Inventory::Inventory(Character* owner)
+	:Owner(owner)
+{
+	ItemMap.reserve(static_cast<size_t>(ItemIdx::ItemMax));
+}
+
+Inventory::~Inventory()
+{
+	ItemMap.clear();
+	Owner = nullptr;
+}
+
+void Inventory::AddItem(ItemIdx itemIdx, int count)
+{
+	if (itemIdx >= ItemIdx::ItemMax)
+	{
+		return;
+	}
+
+	if (count <= 0)
+	{
+		return;
+	}
+
+	// 효과 매핑 임시 (DataTable이 필요하지만 임시로 itemidx = effectIdx 적용)
+	if (ItemMap.find(itemIdx) == ItemMap.end())
+	{
+		ItemMap.insert(make_pair(itemIdx, Item{ count,static_cast<EffectIdx>(itemIdx) }));
+	}
+	else
+	{
+		ItemMap[itemIdx].ItemCount += count;
+	}
+}
+
+bool Inventory::UseItem(ItemIdx itemIdx, Character* Target)
+{
+	if (itemIdx >= ItemIdx::ItemMax)
+	{
+		return false;
+	}
+
+	if (!IsValid(Target))
+	{
+		return false;
+	}
+
+	// 없는 아이템
+	if (ItemMap.find(itemIdx) == ItemMap.end())
+	{
+		return false;
+	}
+
+	// 개수가 모자라다
+	if (ItemMap[itemIdx].ItemCount <= 0)
+	{
+		return false;
+	}
+	
+	bool result = Effect(ItemMap[itemIdx].Effect, Target);
+	
+	if (result)
+	{
+		// 사용 성공
+		ItemMap[itemIdx].ItemCount--;
+	}
+
+	return result;
+}
+
+bool Inventory::Effect(EffectIdx effectIdx, Character* Target)
+{
+	// 원래는 enum 등으로 실패 원인을 반환하는 것이 정석이지만 임시용 bool
+	if (effectIdx >= EffectIdx::EffectMax)
+	{
+		return false;
+	}
+
+	if (!IsValid(Target))
+	{
+		return false;
+	}
+
+	// 이 방식은 현재 effect 개수 등이 많지 않기에 사용
+	// 차후 effect가 늘어날수록 해당 방식은 개선되어야 하며
+	// effect map 등을 이용하여 별도의 effect class 구현이 나을듯 하다
+	switch (effectIdx)
+	{
+	case HealHp:
+	{
+		if (Target->FullHp())
+			return false;
+
+		Target->HealHp(20);
+	}
+		break;
+	case ManaRefrain:
+	{
+		if (Target->FullMp())
+			return false;
+
+		Target->RefreshMp(20);
+	}
+		break;
+	}
+
+	return true;
+}
---
+++ Task1/Inventory.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include"Enums.h"
+#include<unordered_map>
+
+using namespace std;
+
+class Character;
+
+struct Item
+{
+	int ItemCount; // 아이템 개수
+	EffectIdx Effect; // 효과
+};
+
+class Inventory
+{
+public:
+	Inventory(Character* owner);
+	virtual ~Inventory();
+
+public:
+	void AddItem(ItemIdx itemIdx, int count);
+	bool UseItem(ItemIdx itemIdx, Character* Target);
+
+protected:
+	bool Effect(EffectIdx effectIdx, Character* Target);
+
+protected:
+	unordered_map<ItemIdx, Item> ItemMap;
+
+	Character* Owner;
+};
