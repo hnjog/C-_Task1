69874b10986394d4003561214d24fd0014872948
Hyun <hnjogo@gmail.com>
2025-08-11 12:26:50 +0900

    player class 추가

---
+++ Task1/Player.cpp
@@ -0,0 +1,46 @@
+﻿#include "Player.h"
+#include "Inventory.h"
+#include "Util.h"
+
+Player::Player()
+	:Character(),
+	MyInventory(new Inventory(this))
+{
+}
+
+Player::Player(int maxHp, int maxMp, int attack, int defense, string name)
+	:Character(maxHp, maxMp, attack, defense, name),
+	MyInventory(new Inventory(this))
+{
+}
+
+Player::Player(const Stats& stats, string name)
+	:Character(stats,name),
+	MyInventory(new Inventory(this))
+{
+}
+
+Player::~Player()
+{
+	SafeDelete(MyInventory);
+}
+
+void Player::GetItem(ItemIdx item, int count)
+{
+	if (IsValid(MyInventory))
+	{
+		return;
+	}
+
+	MyInventory->AddItem(item, count);
+}
+
+void Player::UseItem(ItemIdx item)
+{
+	if (IsValid(MyInventory))
+	{
+		return;
+	}
+
+	MyInventory->UseItem(item, this);
+}
---
+++ Task1/Player.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Character.h"
+#include "Enums.h"
+
+class Inventory;
+
+class Player :
+    public Character
+{
+public:
+	Player();
+	Player(int maxHp, int maxMp, int attack, int defense, string name);
+	Player(const Stats& stats, string name);
+	virtual ~Player();
+
+public:
+	void GetItem(ItemIdx item, int count);
+	void UseItem(ItemIdx item);
+
+protected:
+	Inventory* MyInventory;
+
+};
+
