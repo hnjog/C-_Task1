d0982e95477cdc90671a05579d6741d866db3501
Hyun <hnjogo@gmail.com>
2025-08-11 13:41:54 +0900

    Enums.h 에 pragma once 추가

--- Task1/Enums.h
+++ Task1/Enums.h
@@ -1,17 +1,18 @@
-﻿
+﻿#pragma once
+
 // 실제 개발시에는 이 둘에 대한 매핑이 더 잘되어 있어야 한다
 enum ItemIdx
 {
 	HealthPotion,
 	ManaPotion,
-	ItemMax
+	ItemMax,
 };
 
 enum EffectIdx
 {
 	HealHp,
 	ManaRefrain,
-	EffectMax
+	EffectMax,
 };
 
 enum SkillIdx
